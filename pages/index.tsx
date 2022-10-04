0e94d082662fdfdb991f24985d23374c45aa32d5
Elu Gonzalez <elugon10@gmail.com>
2022-10-04 17:16:58 +0200

    navbar done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Elu Gonzalez Portfolio</title>
       </Head>
 
-      <h1>Lets build an swesome portfolio</h1>
-    </div>
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+
+
+      </div>
   )
 }
 
