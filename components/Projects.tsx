bc7f0d1bd865e0a5682c3baa6cbfefa95b935233
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2022-10-07 19:02:16 +0200

    customize scollbars done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,7 +16,7 @@ export default function Projects({}: Props) {
             Projects
         </h3>
 
-        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
             {projects.map((project,i)=>( 
                 <div key={i} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 md:p-44 h-screen'>
                     <motion.img
