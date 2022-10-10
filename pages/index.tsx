b6dfd5a3fbbafa29374052594c6e49ef967f2dde
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2022-10-10 21:37:02 +0200

    bug index fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
     <div 
     className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
       <Head>
-        <title>Elu's Portfolio</title>
+        <title>Elu Gonzalez Portfolio</title>
       </Head>
       <Header />
 
