db89fa0ba7f705e01422c6c2e6ccc4d32d3280b2
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2022-10-10 23:33:15 +0200

    css improve

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className='pt-14 sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
         <motion.div 
         initial={{
             x:-500,
