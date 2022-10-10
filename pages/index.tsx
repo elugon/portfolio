58813ac3273f295ba9d97d57eda727b3c3098a73
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2022-10-10 23:32:02 +0200

    css improve

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,9 @@ import Projects from '../components/Projects'
 import ContactMe from '../components/ContactMe'
 
 const Home: NextPage = () => {
+
+  
+
   return (
     <div 
     className='bg-[rgb(36,36,36)] text-white my-screen p-2 snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
