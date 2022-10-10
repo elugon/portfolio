e7b2a22d797db908b1a506a54d2c6581bc87c625
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2022-10-11 00:10:17 +0200

    css improve

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -13,7 +13,7 @@ export default function Experience({}: Props) {
     whileInView={{opacity:1}}
     transition={{duration:1.8}}
     className='my-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[15px] text-gray-500 text-2xl'>
+        <h3 className='absolute top-20 uppercase tracking-[15px] text-gray-500 text-2xl'>
             Experience
         </h3>
 
