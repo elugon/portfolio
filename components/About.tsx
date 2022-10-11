00e29c453f5a774ab480546591d501bed3c020d0
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2022-10-11 12:24:02 +0200

    css fix

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ export default function About({}: Props) {
     initial={{opacity:0}}
     whileInView={{opacity:1}}
     transition={{duration:1.8}}
-    className='flex flex-col relative my-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    className='flex flex-col relative my-screen text-center md:text-left md:flex-row max-w-7xl sm:px-10 px-2 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
         <motion.div
         initial={{
@@ -30,7 +30,7 @@ export default function About({}: Props) {
         </motion.div>
         <div className='mt-6 md:mt-10 space-y-10 px-0 md:px-10'>
             <h4 className='text-base font-semibold sm:text-lg md:text-xl lg:text-2xl'>Here is a little background</h4>
-            <p className='text-xs sm:text-base md:text-lg lg:text-xl'>
+            <p className='text-xs sm:text-base md:text-lg lg:text-xl w-full'>
             <span className='decoration-[#F7AB0A]/50 underline'>Systems Engineer.</span> I have extensive experience in the design, programming, implementation and maintenance of IT applications and a global perspective of the main branches of computer science which gives me a multidisciplinary vision in many fields of engineering. This allows me to have a good ability to analyze and solve problems of many types.
          </p>
         </div>
