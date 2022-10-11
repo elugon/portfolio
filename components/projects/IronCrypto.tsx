2e8f33d12eb873c0ada5f080eab75d8878abfab8
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2022-10-11 11:11:14 +0200

    readme fix

--- components/projects/IronCrypto.tsx
+++ components/projects/IronCrypto.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 export default function IronCrypto({}: Props) {
   return (
-    <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 mt-10 md:p-44 h-screen'>
+    <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 mt-2 md:p-44 h-screen'>
     <motion.img
         initial={{
             y:-300
@@ -20,18 +20,18 @@ export default function IronCrypto({}: Props) {
         viewport={{
             once:true
         }}
-        src='https://github.com/elugon/IronCrypto/raw/dev/public/img/IronCryptoLayout.png'
+        src='https://github.com/elugon/IronCrypto/raw/master/public/img/IronCryptoLayout1.png'
         alt=''
         className='max-h-26 sm:max-h-38 md:max-h-80 rounded'
     />
     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-        <h4 className='text-xl md:text-4xl font-semibold text-center'>
+        <h4 className='text-sm sm:text-xl md:text-4xl font-semibold text-center'>
             <span className='underline decoration-[#F7AB0A]/50'>
             IronCrypto
             </span>
         </h4>
         <p className='text-xs sm:text-md md:text-lg text-center md:text-left'>
-        A web application (Mobile First) developed during the bootcamp at IronHack, designed to visualize the data of the main cryptocurrencies in the market, it has been made with MongoDB - Express - Mongoose - Handlebars and the data is consulted to the CoinGecko API.        </p>
+        A web application (Mobile First) developed during the bootcamp at IronHack, designed to visualize the data of the main cryptocurrencies in the market, it has been made with MongoDB and Handlebars.        </p>
         <p className='space-x-2 text-xs sm:text-md md:text-lg text-center underline decoration-[#F7AB0A]/50'>
         <a href="https://github.com/elugon/IronCrypto" target="_blank" rel="noopener noreferrer">
         Github
