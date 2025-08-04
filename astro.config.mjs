058caada1b265ae7a460d97b3ca3b2f074b4c0ef
elugon <elugon10@gmail.com>
2025-08-05 00:14:13 +0200

    Actualización de .gitignore para incluir la carpeta .astro y el archivo CLAUDE.md. Modificación de la configuración de Astro para definir el sitio, establecer la salida como estática y ajustar la configuración de construcción de activos.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://elugonzalez.dev',
+  integrations: [react()],
+  output: 'static',
+  adapter: undefined,
+  build: {
+    assets: '_astro'
+  }
 });
